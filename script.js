0683828b8d8f198dd53483be65c0eff43f4ee819
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-06 08:50:47 -0400

    Inital Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Create a function that returns true when
+num1 is equal to num2; otherwise return false.*/
+
+function equalNumbers(num1, num2){
+    if(num1 == num2) {
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(equalNumbers(12,17))
